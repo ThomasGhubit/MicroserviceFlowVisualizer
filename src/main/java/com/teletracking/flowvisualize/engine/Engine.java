576a5f1bb9f28c7d7f1fa06de1beb6fa8f99f854
Eric Krohnfeldt <eric-krohnfeldt@teletracking.com>
2020-10-13 16:50:38 -0500

    Add sdf definition to engine interface

--- src/main/java/com/teletracking/flowvisualize/engine/Engine.java
+++ src/main/java/com/teletracking/flowvisualize/engine/Engine.java
@@ -1,7 +1,9 @@
 package com.teletracking.flowvisualize.engine;
 
+import com.teletracking.flowvisualize.parser.SdfDefinition;
+
 public interface Engine {
 
-    GraphedModel buildModel();
+    GraphedModel buildModel( SdfDefinition definition );
 
 }
