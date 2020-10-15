a5f0ea8e77376cf128b04f806cef77cf107d2d2f
EvanSnapp <evan-snapp@teletracking.com>
2020-10-15 14:51:05 -0500

    add graph mapper

--- src/main/java/com/teletracking/flowvisualize/middleware/GraphResponse.java
+++ src/main/java/com/teletracking/flowvisualize/middleware/GraphResponse.java
@@ -1,5 +1,9 @@
 package com.teletracking.flowvisualize.middleware;
 
+import com.teletracking.flowvisualize.engine.Edge;
+import com.teletracking.flowvisualize.engine.GraphedModel;
+import com.teletracking.flowvisualize.engine.Node;
+
 class DisplayInfo {
     public int repulsion = 2600;
     public double friction = 0.5;
@@ -11,11 +15,30 @@ public class GraphResponse {
     public String  _id = "Viz";
     public String  _rev = "1-e8ceaebda42a2c5a76d6684ff9d867da";
     public DisplayInfo sys = new DisplayInfo();
-    public String src;
+    public String src = "";
     public String example= "cyoa";
     public String title = "Viz";
 
     public GraphResponse(String source) {
         this.src = source;
+    }
+
+    public GraphResponse(GraphedModel model) {
+        StringBuilder src_builder = new StringBuilder();
+
+        for (Edge edge : model.getEdges()) {
+            src_builder.append(String.format("%s -> %s\n", edge.getSource(), edge.getDestination()));
+        }
+        src_builder.append("\n");
+        for (Node node : model.getNodes()){
+            String attributes = "{";
+            for (String attr : node.getAttributes().keySet()) {
+                attributes += String.format("%s:%s, ", attr, node.getAttributes().get(attr));
+            }
+            attributes = attributes.substring(0, attributes.length()-2) + "}";
+            src_builder.append(String.format("%s {%s}\n", node.getName(), attributes));
+        }
+
+        this.src = src_builder.toString();
     }
 }
