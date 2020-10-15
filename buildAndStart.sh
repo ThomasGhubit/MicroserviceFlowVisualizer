978759b16ad40dc6d0b76604e1412b663fa693a3
Srujan Santhosh <srujan-santhosh@teletracking.com>
2020-10-15 12:19:14 -0500

    modified UI

--- buildAndStart.sh
+++ buildAndStart.sh
@@ -4,4 +4,4 @@ set -e
 
 mvn clean install
 docker build -t microserviceflowvisualizer .
-docker run -t --rm --name microserviceflowvisualizer -p 8080:8080 microserviceflowvisualizer
+docker run -t --rm --name microserviceflowvisualizer -p 8081:8080 microserviceflowvisualizer
